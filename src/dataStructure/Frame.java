225d3b14cb619932b27d0a9d21292896f3e3bf71
MagicKriss <krisjanis.kallings@gmail.com>
2017-02-15 17:50:04 +0200

    Triangles

--- src/dataStructure/Frame.java
+++ src/dataStructure/Frame.java
@@ -19,36 +19,32 @@ public class Frame {
 	public int[] klatt_param;
 	public int[] spect_data;
 
-	
-	 static int default_freq[] = {200,500,1200,3000,3500,4000,6900,7800,9000}; 
-	 static int default_width[] = {750,500,550,550,600,700,700,700,700}; 
-	 static int default_klt_bw []= {89,90,140,260,260,260,500,500,500};
-	 
-	
-	
+	static int default_freq[] = { 200, 500, 1200, 3000, 3500, 4000, 6900, 7800,
+			9000 };
+	static int default_width[] = { 750, 500, 550, 550, 600, 700, 700, 700, 700 };
+	static int default_klt_bw[] = { 89, 90, 140, 260, 260, 260, 500, 500, 500 };
+
 	public void frameLoader(ByteArrayInputStream inRead, int file_format) {
 		formants = new int[9][2];
 		// There are 7 elements in SPECTSQ2 files, only 4 in TSEQ and TSEK
 		peaks = new int[9][7];
-		klatt_param=new int[14];
-		
-		for(int i=0; i<9; i++)
-		   {
-				formants[i][0] = 0;
-				peaks[i][0] = default_freq[i];
-				peaks[i][1] = 0;
-				peaks[i][2] = default_width[i];
-				peaks[i][3] = default_width[i];
-				peaks[i][4] = default_klt_bw[i];
-				peaks[i][5] = 0;
-				peaks[i][6] = default_klt_bw[i];
-		   }
+		klatt_param = new int[14];
+
+		for (int i = 0; i < 9; i++) {
+			formants[i][0] = 0;
+			peaks[i][0] = default_freq[i];
+			peaks[i][1] = 0;
+			peaks[i][2] = default_width[i];
+			peaks[i][3] = default_width[i];
+			peaks[i][4] = default_klt_bw[i];
+			peaks[i][5] = 0;
+			peaks[i][6] = default_klt_bw[i];
+		}
 
 		byte[] buffer = new byte[10];
 		inRead.read(buffer, 0, 8);
 		time = Phoneme.byteWrapper(buffer);
 		inRead.read(buffer, 0, 2);
-
 
 		inRead.read(buffer, 0, 8);
 		pitch = Phoneme.byteWrapper(buffer);
@@ -82,31 +78,24 @@ public class Frame {
 
 			inRead.read(buffer, 0, 2);
 			formants[i][0] = Phoneme.byteWrapper(buffer);
-			
 			inRead.read(buffer, 0, 2);
 			formants[i][1] = Phoneme.byteWrapper(buffer);
-
 			inRead.read(buffer, 0, 2);
 			peaks[i][0] = Phoneme.byteWrapper(buffer);
-
 			inRead.read(buffer, 0, 2);
 			peaks[i][1] = Phoneme.byteWrapper(buffer);
-
 			inRead.read(buffer, 0, 2);
 			peaks[i][2] = Phoneme.byteWrapper(buffer);
-
 			inRead.read(buffer, 0, 2);
 			peaks[i][3] = Phoneme.byteWrapper(buffer);
-
 			if (file_format == 2) {
 				inRead.read(buffer, 0, 2);
 				peaks[i][4] = Phoneme.byteWrapper(buffer);
-
 				inRead.read(buffer, 0, 2);
 				peaks[i][5] = Phoneme.byteWrapper(buffer);
-
 				inRead.read(buffer, 0, 2);
 				peaks[i][6] = Phoneme.byteWrapper(buffer);
+
 			}
 		}
 
@@ -128,5 +117,8 @@ public class Frame {
 
 	public int[][] getPeaks() {
 		return peaks;
+	}
+	public int[][] getFormants() {
+		return formants;
 	}
 }
--- src/dataStructure/Graph.java
+++ src/dataStructure/Graph.java
@@ -4,9 +4,12 @@ package dataStructure;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Polygon;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Point2D;
+import java.awt.geom.QuadCurve2D;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -24,7 +27,7 @@ public class Graph {
 	private JTabbedPane tabbedPaneGraphs;
 	private JPanel filePanel;
 	// mapButtons will collect for each button it own frame
-	//private Map<JButton, Frame> mapButtons;
+	// private Map<JButton, Frame> mapButtons;
 	private Map<JPanel, Frame> mapPanels;
 
 	public Graph(String fileName, ArrayList<Frame> frameList) {
@@ -47,56 +50,53 @@ public class Graph {
 
 			Frame currentFrame = frameList.get(i);
 
-	/*	JButton jButton = new JButton("" + i);
-			jButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					loadFrame((JButton) arg0.getSource());
-				}
-			});
+			/*
+			 * JButton jButton = new JButton("" + i);
+			 * jButton.addActionListener(new ActionListener() { public void
+			 * actionPerformed(ActionEvent arg0) { loadFrame((JButton)
+			 * arg0.getSource()); } });
+			 * 
+			 * jButton.setBounds(10, y, 400, 25);
+			 */
 
-			jButton.setBounds(10, y, 400, 25);
-
-			*/
-			
-			
-			JPanel sin = new DrawGraph(currentFrame);
-			sin.setBounds(10,y,500,100);
+			JPanel sin = new DrawTriangle(currentFrame);
+			sin.setBounds(10, y, 900, 100);
 			sin.setBackground(Color.WHITE);
 			sin.setVisible(true);
 			y += 105;
 
 			sin.addMouseListener(new MouseListener() {
-                public void mouseClicked(MouseEvent e) {
-                        loadFrame((JPanel) e.getSource());
-                }
+				public void mouseClicked(MouseEvent e) {
+					loadFrame((JPanel) e.getSource());
+				}
 
-                @Override
-                public void mouseEntered(MouseEvent arg0) {
-                        // TODO Auto-generated method stub
-                        
-                }
+				@Override
+				public void mouseEntered(MouseEvent arg0) {
+					// TODO Auto-generated method stub
 
-                @Override
-                public void mouseExited(MouseEvent arg0) {
-                        // TODO Auto-generated method stub
-                        
-                }
+				}
 
-                @Override
-                public void mousePressed(MouseEvent arg0) {
-                        // TODO Auto-generated method stub
-                        
-                }
+				@Override
+				public void mouseExited(MouseEvent arg0) {
+					// TODO Auto-generated method stub
 
-                @Override
-                public void mouseReleased(MouseEvent arg0) {
-                        // TODO Auto-generated method stub
-                        
-                }
-        });
+				}
+
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void mouseReleased(MouseEvent arg0) {
+					// TODO Auto-generated method stub
+
+				}
+			});
 
 			filePanel.add(sin);
-			//filePanel.add(jButton);
+			// filePanel.add(jButton);
 			mapPanels.put(sin, currentFrame);
 
 		}
@@ -106,35 +106,76 @@ public class Graph {
 		tabbedPaneGraphs.setSelectedComponent(filePanel);
 
 	}
-	class DrawGraph extends JPanel {
+
+	class DrawTriangle extends JPanel {
 		public Frame currentFrame;
-		public DrawGraph(Frame currentFrame) {
+
+		public DrawTriangle(Frame currentFrame) {
 			this.currentFrame = currentFrame;
-			
-			
+
 		}
 
+		protected void paintComponent(Graphics g) {
+			int[][] peaks = currentFrame.getPeaks();
+			super.paintComponent(g);
 
-	    protected void paintComponent(Graphics g) 
-	    {
-	    	int[][] peaks = currentFrame.getPeaks();
-	        super.paintComponent(g);
+			/*
+			 * Polygon p = new Polygon();
+			 * 
+			 * for (int i =0; i <8; i++) { // for length add points
+			 * p.addPoint(peaks[i][0]/20,-(peaks[i][1] >> 6)+100);
+			 * 
+			 * }
+			 * 
+			 * 
+			 * g.setColor(Color.GREEN); g.drawPolyline(p.xpoints, p.ypoints,
+			 * p.npoints);
+			 */
+			// Polygon p = new Polygon();
+			int[] x = new int[3];
+			int[] y = { 100, 0, 100 };
+			int[][] points = new int[8][4];
+			for (int i = 0; i < 8; i++) {
+				points[i][0] = peaks[i][0] / 25; // peak x value
+				points[i][1] = -(peaks[i][1] >> 6) / 2 + 100; // peak y value
+				points[i][2] = (peaks[i][2] / 25 / 2); // left x
+				points[i][3] = (peaks[i][3] / 25 / 2); // right x
 
+				x[0] = points[i][0] - points[i][2];
+				x[1] = points[i][0];
+				x[2] = points[i][0] + points[i][3];
+				y[1] = points[i][1];
+				g.setColor(Color.ORANGE);
+				Polygon poly = new Polygon(x, y, 3);
+				g.fillPolygon(poly);
+				g.setColor(Color.RED);
+				g.drawPolygon(poly);
 
-	        Polygon p = new Polygon(); 
+			}
+		}
 
-	       for (int i =0; i <8; i++) { // for length add points 
-	            p.addPoint(peaks[i][0]/20,-(peaks[i][1] >> 6)+100);
-
-	        }
-
-
-	        g.setColor(Color.GREEN);
-	        g.drawPolyline(p.xpoints, p.ypoints, p.npoints);
-
-	    }
-	 }
+	}
 	
+	public void drawWave(Frame currentFrame){
+		int  pt;
+		int  peak;
+		int  ix;
+		double x0, x1;
+		int  y0, y1;
+		int  x, x2, x3;
+		double xinc;
+		double yf;
+		int font_height;
+		double dx = currentFrame.dx;
+		double nx = currentFrame.nx;
+		int[] pk;
+		
+		xinc = dx;
+		x0 = xinc;
+		x1 = nx * xinc;
+		//if(selected){
+		//pk = currentFrame.getPeaks()[pk_select];// FIXME pk_select needs to be iniciated
+	}
 	
 
 	public JPanel getjPanelOfGraph() {
@@ -144,8 +185,7 @@ public class Graph {
 	// to get first element in mapButtons
 	public void loadFirstFrame() {
 		// load first frame
-		Map.Entry<JPanel, Frame> entry = mapPanels.entrySet().iterator()
-				.next();
+		Map.Entry<JPanel, Frame> entry = mapPanels.entrySet().iterator().next();
 		loadFrame(entry.getKey());
 	}
 
