04e5e85cc5ece5cd620fc8be41df76629bc7c5b3
Valdis Vitolins <valdis.vitolins@odo.lv>
2017-02-26 23:17:59 +0200

    JNI update script changed to use ld instead with *.o files instead of gcc

--- jni/updateJNIchanges.sh
+++ jni/updateJNIchanges.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-pathToEspeakDir=/home/student/workspace-c/espeak-ng
-pathToJedit=/home/student/workspace/eSpeak-Jedit
 pdir=$(pwd)
 cdir=$(dirname "$0")
-if [ $cdir != "." ]; then
-  cd $cdir # change to current directory
+cd $cdir # change to script directory
+cdir=$(pwd)
+echo $cdir
+espeakDir=${cdir}/../../espeak-ng
+if [ ! -d ${espeakDir} ]; then
+ echo "eSpeak NG directory was not found!"
+ exit 1
 fi
 cd ../src
-echo "Compiling ESpeakService.java.."
+echo "Compiling ESpeakService.java..."
 javac eSpeakServices/ESpeakService.java
-echo "(Re)Generate eSpeakServices_ESpeakService.h in jni/include.."
+echo "Generating eSpeakServices_ESpeakService.h in jni/include..."
 javah -jni -d ../jni/include eSpeakServices.ESpeakService
-echo "(Re)Compiling eSpeakService.c .."
-gcc -Wall -fPIC -O -g -I/usr/lib/jvm/java-8-openjdk-amd64/include -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -I/usr/include -I/usr/include/x86_64-linux-gnu -I/usr/lib/gcc/x86_64-linux-gnu/6/include -I/usr/lib/gcc/x86_64-linux-gnu/6/include-fixed -I/usr/local/include -I${pathToEspeakDir}  ${pathToJedit}/jni/src/eSpeakService.c -c -o ${pathToJedit}/jni/src/eSpeakService.o
-echo "(Re)Compiling libespeakservice.so shared library .."
-gcc -shared ${pathToJedit}/jni/src/eSpeakService.o -L${pathToJedit}/jni/lib -lespeak-ng-fork -o ${pathToJedit}/jni/lib/libespeakservice.so 
-echo "hopefully that is done"
+echo "Compiling eSpeakService.c..."
+gcc -Wall -fPIC -O -g -I/usr/lib/jvm/java-8-openjdk-amd64/include -I/usr/lib/jvm/java-8-openjdk-amd64/include/linux -I/usr/include -I/usr/include/x86_64-linux-gnu -I/usr/lib/gcc/x86_64-linux-gnu/6/include -I/usr/lib/gcc/x86_64-linux-gnu/6/include-fixed -I/usr/local/include -I${espeakDir}  ${cdir}/src/eSpeakService.c -c -o ${cdir}/src/eSpeakService.o
+echo "Compiling libespeakservice.so shared library..."
+#gcc -shared ${cdir}/src/eSpeakService.o -L${cdir}/lib -lespeak-ng-fork  $(find ${espeakDir} -name "*.o"|tr "\n" " ") -o ${cdir}/lib/libespeakservice.so
+ld --allow-multiple-definition -G ${cdir}/src/eSpeakService.o $(find /home/valdis/code/espeak-ng -name "*.o"|tr "\n" " ") -o ${cdir}/lib/libespeakservice.so
+echo "OK"
 cd $pdir  # go bach to previous directory
