1b33abfae9daa736430e393367f3aafbf7034070
Valdis Vitolins <valdis.vitolins@odo.lv>
2018-03-18 17:23:11 +0200

    Fix format

--- src/main/java/org/espeakng/jeditor/jni/ESpeakService.java
+++ src/main/java/org/espeakng/jeditor/jni/ESpeakService.java
@@ -1,6 +1,6 @@
 package org.espeakng.jeditor.jni;
 
-/**
+/*-
  * 	EspeakService class is used to call native EspeakNG functions
  * 		EspeakService loads libespeakservice.so library witch calls EspeakNG public library functions and 
  *  	use JNI to return values back to this Java class. At this stage libespeakservice.so library
@@ -39,15 +39,12 @@ package org.espeakng.jeditor.jni;
  *		===============================================
  *		After changes have been made, EspeakNG should be rebuild ( just rerun updateJNIchanges.sh script ). 
  * 		
- * */
+ */
 public class ESpeakService {
 
 	static {
 		try {
 			System.load(System.getProperty("user.dir") + "/.lib/libespeakservice.so");
-//			System.load(System.getProperty("user.dir") + "/src/main/resources/eSpeakService.o");
-			// System.out.println(System.getProperty("user.dir") +
-			// "/lib/libespeakservice.so");
 		} catch (UnsatisfiedLinkError e) {
 			System.out.println("Native code library failed to load.\n" + e);
 			System.exit(1);
@@ -55,28 +52,34 @@ public class ESpeakService {
 	}
 
 	/** Function returns string with Espeak-NG version */
-	public static native String nativeGetEspeakNgVersion();  
-	
-	/** Function loads SpectSeq from a given file to a passed SpectSeq object.
-	 * 		Function returns 0 (ENS_OK) if was successful, otherwise it returns non 0 integer value (espeak_ng_STATUS)
-	 *  */
+	public static native String nativeGetEspeakNgVersion();
+
+	/**
+	 * Function loads SpectSeq from a given file to a passed SpectSeq object.
+	 * Function returns 0 (ENS_OK) if was successful, otherwise it returns non 0
+	 * integer value (espeak_ng_STATUS)
+	 */
 	public static native int nativeGetSpectSeq(SpectSeq spect, String fileName);
 
-	
-	/** Function returns array of phoneme strings which was got by calling EspeakNG espeak_TextToPhonemes with passed string as parameter.
-	 * 		As EspeakNG espeak_TextToPhonemes function returns phonemes for the text up to end of a sentence, or comma, semicolon, colon, 
-	 * 		or similar punctuation, espeak_TextToPhonemes function is called on remained untranslated string in loop (if needed), till all given text 
-	 * 		has been translated. Every string in returned array represents one such call.
-	 * 		
-	 * 		EspeakNG espeak_TextToPhonemes works only after initialization and language has been selected, so Language is not optional.
-	 *  */
+	/**
+	 * Function returns array of phoneme strings which was got by calling
+	 * EspeakNG espeak_TextToPhonemes with passed string as parameter. As
+	 * EspeakNG espeak_TextToPhonemes function returns phonemes for the text up
+	 * to end of a sentence, or comma, semicolon, colon, or similar punctuation,
+	 * espeak_TextToPhonemes function is called on remained untranslated string
+	 * in loop (if needed), till all given text has been translated. Every
+	 * string in returned array represents one such call.
+	 * 
+	 * EspeakNG espeak_TextToPhonemes works only after initialization and
+	 * language has been selected, so Language is not optional.
+	 */
 	public static native String[] nativeTextToPhonemes(String textToTranslate, String language);
-	
-	/** 
-	 *	Language defaults to "en"
-	 * */
-	public static String[] nativeTextToPhonemes(String textToTranslate){
-		return nativeTextToPhonemes(textToTranslate, "en"); 
+
+	/**
+	 * Language defaults to "en"
+	 */
+	public static String[] nativeTextToPhonemes(String textToTranslate) {
+		return nativeTextToPhonemes(textToTranslate, "en");
 	}
 
 }
