58ad2ca0d671cb18fed9837dc24d15a90f1bae2f
Anete Klavina <aneteklavinaa@gmail.com>
2018-08-23 15:52:58 +0300

    Simplified example which takes data as parameters from command line

--- src/main/c/speak.c
+++ src/main/c/speak.c
@@ -2,48 +2,31 @@
 #include <malloc.h>
 #include <espeak-ng/speak_lib.h>
 
-
-
 espeak_POSITION_TYPE position_type;
 espeak_AUDIO_OUTPUT output;
-char *path=NULL;
-int Buflength = 500, Options=0;
+char *path = NULL;
+int buflength = 500, options = 0;
 void* user_data;
-t_espeak_callback *SynthCallback;
-espeak_PARAMETER Parm;
 
+unsigned int size, position = 0, end_position = 0, flags = espeakCHARS_AUTO,
+		*unique_identifier;
 
-
-//char Voice[] = {"lt+klatt2"};
- char Voice[] = {"English"};
-
-char text[30] = {"this is a english test"};
-unsigned int Size,position=0, end_position=0, flags=espeakCHARS_AUTO, *unique_identifier;
-
-
-
-
-int main(int argc, char* argv[] ) 
-{
-    output = AUDIO_OUTPUT_PLAYBACK;
-    int I, Run = 1, L;    
-    espeak_Initialize(output, Buflength, path, Options ); 
-    espeak_SetVoiceByName(Voice);
-    //const char *langNativeString = "lt"; //Default to US English
-    const char *langNativeString = "en"; //Default to US English
-    espeak_VOICE voice;
-    memset(&voice, 0, sizeof(espeak_VOICE)); // Zero out the voice first
-    voice.languages = langNativeString;
-    //voice.name = "klatt";
-    voice.name = "US";
-    voice.variant = 2;
-    voice.gender = 1;
-    espeak_SetVoiceByProperties(&voice);
-    Size = strlen(text)+1;    
-    printf("Saying  '%s'",text);
-    espeak_Synth( text, Size, position, position_type, end_position, flags,
-    unique_identifier, user_data );
-    espeak_Synchronize( );
-    printf("\n:Done\n"); 
-    return 0;
+int main(int argc, char* argv[]) {
+	printf("language:%s text:%s\n", argv[1], argv[2]);
+	output = AUDIO_OUTPUT_PLAYBACK;
+	espeak_Initialize(output, buflength, path, options);
+	espeak_VOICE voice;
+	memset(&voice, 0, sizeof(espeak_VOICE)); // Zero out the voice first
+	voice.languages = argv[1];
+	voice.name = argv[1];
+	voice.variant = 1;
+	voice.gender = 1;
+	espeak_SetVoiceByProperties(&voice);
+	size = strlen(argv[2]) + 1;
+	printf("Saying  '%s'", argv[2]);
+	espeak_Synth(argv[2], size, position, position_type, end_position, flags,
+			unique_identifier, user_data);
+	espeak_Synchronize();
+	printf("\n:Done\n");
+	return 0;
 }
