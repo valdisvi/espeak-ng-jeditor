81e8aa9b7bd5cab91eabffc25860a538303003d0
Andrejs Freiss <hobijs3@inbox.lv>
2019-08-30 14:30:47 +0300

    Additional fix for JUnit tests

--- src/test/java/org/espeakng/jeditor/gui/AllTests.java
+++ src/test/java/org/espeakng/jeditor/gui/AllTests.java
@@ -1,4 +1,7 @@
 package org.espeakng.jeditor.gui;
+//package org.espeakng.jeditor.data;
+//package org.espeakng.jeditor.jni;
+//package org.espeakng.jeditor.utils;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,16 +13,28 @@ import org.espeakng.jeditor.gui.VoiceMenuItemTest;
 import org.espeakng.jeditor.gui.OptionsMenuItemTest;
 import org.espeakng.jeditor.gui.ToolsMenuItemTest;
 import org.espeakng.jeditor.gui.HelpMenuItemTest;
+import org.espeakng.jeditor.data.GraphTest;
+import org.espeakng.jeditor.data.VowelChartTest;
+import org.espeakng.jeditor.jni.ESpeakServiceTest;
+import org.espeakng.jeditor.jni.TextFieldsHasRightValues;
+import org.espeakng.utils.CommandUtilitiesTest;
+import org.espeakng.utils.ProcessThreadTest;
 
 @RunWith(Suite.class)
 // Keep HelpMenuItemTest last as it opens a webpage and breaks every other test that comes after it
-@SuiteClasses({ 
+@SuiteClasses({
+	GraphTest.class,
+	VowelChartTest.class,
+	ESpeakServiceTest.class,
+	TextFieldsHasRightValues.class,
+	CommandUtilitiesTest.class,
+	ProcessThreadTest.class,
 	CompileMenuItemTest.class, 
 	FileMenuItemTest.class, 
-	SpeakMenuItemTest.class, 
 	VoiceMenuItemTest.class,
 	OptionsMenuItemTest.class, 
 	ToolsMenuItemTest.class, 
+	SpeakMenuItemTest.class, 
 	HelpMenuItemTest.class })
 
 public class AllTests {
